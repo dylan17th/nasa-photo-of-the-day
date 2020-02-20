4fc5b34953ea09dd36291b42215d934822c657cb
Dylan Collins <dcollins5590@students.mc3.edu>
2020-02-20 13:16:50 -0500

    finished the project

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./App.css";
 import CardDataLoader from './card-data-loader';
-import styled from 'styled-components';
 
-const NasaBackground = styled.div`
-backgourn-image: url(bryan-goff-IdYsDAsLaj8-unsplash.jpg)
-`
+
+
 
 function App() {
   return (
-    <NasaBackground className="App">
+    <div className="App">
       <CardDataLoader />
-    </NasaBackground>
+    </div>
+
   );
 }
 
--- src/card-layout.js
+++ src/card-layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const NasaCard = styled.div`
-    width:50%;
+    width:35%;
     display:flex;
     align-content:center; 
     text-align:center;
@@ -12,28 +12,52 @@ const NasaCard = styled.div`
     padding: 1rem;
     border-radius:10px;
     background-image: linear-gradient( to top , #d38601, #7f7f7f);
-    color:white;
+    color:black;
     `;
 const NasaImage = styled.img`
 width:100%;
 object-fit: scale;
 flex-shink:down;
-max-width:100%;
-height:500px;
+height:300px;
 border-radius:20px;
-`
+`;
 const Heading = styled.h2`
 font-size:2rem;
+margin:.5rem;
+`;
+const ImageBorder = styled.div`
+border-top: black dashed 2px;
+padding:1rem;
+`;
+const Paragraph = styled.p`
+border-bottom: black dashed 2px;
+padding:1rem;
+margin-top:-1rem;
+line-height:2rem;
+font-size: .7rem;
+`;
+const NasaButton = styled.button`
+width:50%;
+color:white;
+background-color:black;
+padding: 1rem 3rem;
+border-radius:10px; 
+border:none;
+`;
+const NasaAnchor = styled.a`
+text-decoration:none;
+color:white;
 `
-
 export default function layout({title,url,date,explanation}){
     return (
         <NasaCard>
             <Heading>{title}</Heading>
-            <NasaImage src={url} alt='Picture of the day'></NasaImage>
+            <ImageBorder>
+                <NasaImage src={url} alt='Picture of the day'></NasaImage>
+            </ImageBorder>
             <p className='date'>Date: {date} </p>
-            <p>{explanation}</p>
-            <button className='button'><a href='https://api.nasa.gov/#apod'>Visit NASA API Here</a></button>
+            <Paragraph>{explanation}</Paragraph>
+            <NasaButton><NasaAnchor href='https://api.nasa.gov/#apod'>Visit NASA API Here</NasaAnchor></NasaButton>
         </NasaCard>
     )
 }
