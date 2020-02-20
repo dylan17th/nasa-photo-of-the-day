ab49b04c926f2f63960fd156d3c1ccac831f6178
Dylan Collins <dcollins5590@students.mc3.edu>
2020-02-20 13:44:30 -0500

    put in a background for the project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./App.css";
 import CardDataLoader from './card-data-loader';
+import Logo from './logo.jpg';
+import styled from 'styled-components';
+const Background = styled.img`
+position:absolute;
+max-width:100%;
+z-index: -1;
+`
 
 
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+    <Background src={Logo}/>
       <CardDataLoader />
     </div>
 
